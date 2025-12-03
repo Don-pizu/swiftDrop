60bfc898473d2f3b4f95b09b69e4d079ec2554fd
Asiru Adedolapo <105151400+Don-pizu@users.noreply.github.com>
2025-12-03 20:57:49 +0100

    Update redis.js

--- util/redis.js
+++ util/redis.js
@@ -2,19 +2,30 @@
 // redis is a fast in-memory/temporary data store inwhich BullMQ uses to store and track jobs
 //Shared Redis client for BullMQ
 
+
 const { Queue, Worker, QueueScheduler } = require('bullmq');
 const IORedis = require('ioredis');
 
-const redisPort = Number(process.env.REDIS_DB_PORT) || 6379;
+let connectionRedis;
 
+if (process.env.REDIS_URL) {
+  // For Production (Render)
+  connectionRedis = new IORedis(process.env.REDIS_URL, {
+    maxRetriesPerRequest: null,
+    enableReadyCheck: false
+  });
 
-const connectionRedis = new IORedis({   //new IORedis('redis://127.0.0.1:6379');
-  host: process.env.REDIS_HOST || "127.0.0.1",
-  port: redisPort,
-  password: process.env.REDIS_PASS || undefined,    //for development
-  maxRetriesPerRequest: null,   // ✅ REQUIRED for BullMQ
-  enableReadyCheck: false       // ✅ prevents "Ready check" errors
+} else {
+  // Local Development
+  connectionRedis = new IORedis({
+    host: process.env.REDIS_HOST || "127.0.0.1",
+    port: Number(process.env.REDIS_DB_PORT) || 6379,
+    password: process.env.REDIS_PASS || undefined,     //for development
+    maxRetriesPerRequest: null,     // ✅ REQUIRED for BullMQ
+    enableReadyCheck: false         // ✅ prevents "Ready check" errors
 });
+  });
+}
 
 connectionRedis.on('connect', () => console.log('✅ Redis connected successfully!'));
 connectionRedis.on('error', (err) => console.error('❌ Redis connection error:', err));
